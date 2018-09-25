e84127242faf976fa6ee5c346166ff77ab1fc16f
superleon14 <leolgs14@gmail.com>
2018-09-24 21:05:30 -0300

    Se crea el proyecto.

---
+++ AED_Monitor_de_RED_V1/src/aed_monitor_de_red_v1/AED_Monitor_de_RED_V1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aed_monitor_de_red_v1;
+
+/**
+ *
+ * @author leon
+ */
+public class AED_Monitor_de_RED_V1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
